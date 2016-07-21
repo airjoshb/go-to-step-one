cb96e393728d4ab412ef5f87411f9df22633a214
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2016-07-21 10:33:50 -0700

    User browserSync instead of webpack dev server

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1aa963a34d48616f6918df23c9eadfe4f05b7b4853073190a2018dc234019d6
-size 1597
+oid sha256:f6b014e82224028f7bf2aab7c8e0ebd532866432c5941247d67cd472f47e75d8
+size 1397
