5df348bdd68d92af5069f3c8c12e4a7a7158399f
Darin Dimitroff <d@thecrazyones.agency>
2016-11-22 13:07:21 +0200

    Moves cssnext after cssImport
    This allows for the use of a single media-queries files.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03e235a634190ef2feb1093c666cca0f7e7b75bcc011af8fb3ec8e98d773e87a
+oid sha256:71f985acba0da85c7de299c18301c693622766f1386946eee34ac54d22d9bcef
 size 1797
