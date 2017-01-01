a3730270535e41c31dfecd38d363326eac592803
Darin Dimitroff <d@thecrazyones.agency>
2017-01-01 17:04:58 +0200

    Adds SVG sprite to watch task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa3dfd0a1084d6f9e9e009fb39ed22edfd8e9ce778ede8ee2493ed62e9142707
-size 2371
+oid sha256:e8a04dff16207c94e1dd2861efd977276423c8c5d87b9f4f4f10235e31771d92
+size 2427
