26d420a300e76523b2c4dee8eb1e3f6244f0ab13
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-02-21 15:31:05 -0800

    Add preview template for product page

---
+++ src/js/cms-preview-templates/products.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1be880343b43daf461417b7b5195975f267272b8c77c479381d194e9e2b9e186
+size 4220
--- src/js/cms.js
+++ src/js/cms.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:678f8ef39b5439be0cd38ed169f8ba08e494a0d7df84a1ef0dbc0ebe9a730459
-size 181
+oid sha256:b39e007e968c9031d9c8530af9dfa247b18528e63a4f211c526e276ab9a7e3ef
+size 303
