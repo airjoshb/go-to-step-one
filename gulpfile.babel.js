c432000a06f1a7d4b6f24984b5d98a43eb202ff3
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-09-02 14:08:54 -0700

    Setup cms with netlify identity

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4462b9db5eb59fb4fb8bfe37c95de483eeb6fdb4bc3ac650a781b8f8daa6ffde
-size 3166
+oid sha256:b34b1476d3710bef694ce18827d6e5fdbc748429d25131283b790ca78a14331e
+size 2532
