3f43c8193201a5a0c3e778388cd41b1360ff28df
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-09-02 18:01:04 -0700

    Add identity script

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b34b1476d3710bef694ce18827d6e5fdbc748429d25131283b790ca78a14331e
-size 2532
+oid sha256:67c2ea5653a57649992e42e0984ad5ed0d464df4088ab3db5b3a0312a8f5ff48
+size 2722
---
+++ site/static/admin/netlify-identity-umd.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c987ca2cc68cbbc1b1d9cfb43f0a095b8ff09043aea4a3bbe16225df919237e6
+size 194167
