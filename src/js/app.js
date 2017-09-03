f58a2015af1f5d6fa04a5835c227ef805bf7b782
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-09-02 18:58:49 -0700

    Fix token acceptance

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4ada4b67d50574430f8e629fdb9a3ecabccb3d74cf040af3fb92874e499faa1
-size 417
+oid sha256:76951086397333fde10d48bb784683a7b99ffb215a9e6ce6b9b788c12973e879
+size 450
