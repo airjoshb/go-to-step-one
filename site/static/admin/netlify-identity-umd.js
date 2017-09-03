3cb552b10048872f4bf2984850e65f5bb12aa17b
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-09-02 18:55:39 -0700

    Really primitive invite acceptance

--- site/static/admin/netlify-identity-umd.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c7edbe36f928a7a61ee6bff7aa7c6b544250ce49ca5edc956cc31b5c302596e2
-size 197246
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd95b68770be74408c09eb78613bf571373146f68422cb6875abf3d35f5065a8
-size 32
+oid sha256:f4ada4b67d50574430f8e629fdb9a3ecabccb3d74cf040af3fb92874e499faa1
+size 417
