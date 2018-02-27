d6e55b22671a905f1ddd11defb8c931f29b8a4db
talves <tony@3alves.com>
2018-02-26 16:45:28 -0800

    remove unused module

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8082a4cd82a69d69cb1cb4ece5150a2831400c9b7115e34e126e8ef766dd1d2c
-size 2783
+oid sha256:11b0517b510c38563addc63a527c336b96d20b00422b94a17d36078da3821be8
+size 2747
