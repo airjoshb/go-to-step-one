d52d2bafc8ef3050a41ad0752af706beaba7b55a
Joshua Bradley <josh@gotostepone.com>
2019-02-08 13:13:08 -0800

    MVP commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27f7591ceb044ae718bac6d83674ea66f9008b5b4fdda45bae93880d3ef63ca2
-size 242
+oid sha256:9434e3277b9db6c2f35d4004179f6ae628813a4794af0999a7979e1115978b8d
+size 318
